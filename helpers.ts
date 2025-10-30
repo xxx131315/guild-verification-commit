b4d593146f6b49da229dcbe6f46986e1f696350b
xxx131315 <141367674+xxx131315@users.noreply.github.com>
2025-10-30 22:46:27 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T19:18:53.524Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-10-30T19:46:27.156Z
+Commit: Fix CSS styling bug on mobile devices
